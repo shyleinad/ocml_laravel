e1f79e873e8183b9c472f72d5599f148977ada83
shyleinad <turi.daniel@protonmail.com>
2023-08-06 18:30:25 +0200

    maintenances table migration

---
+++ database/migrations/2023_08_06_161535_create_maintenances.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id(); //id of the maintenance record
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //user of the maintenance record
+            $table->foreignId('vehicle_id')->constrained()->onDelete('cascade'); //vehicle of the maintenance record
+            $table->int('mileage'); //milage: when the work was done
+            $table->date('date'); //date: when the work was done
+            $table->string('work_done'); //what was done (oil change for example)
+            $table->string('changed_parts'); //changed parts
+            $table->timestamps(); //record added timestamp
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
