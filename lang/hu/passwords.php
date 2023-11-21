387eabb1d577631a860cd7c5b513ee9c4ab6d160
shyleinad <turi.daniel@protonmail.com>
2023-11-21 11:41:14 +0100

    added lang files

---
+++ lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'A jelszó vissza lett állítva.',
+    'sent' => 'Emailben elküldük a jelszó visszíállító linket.',
+    'throttled' => 'Kárjük várjon mielőtt újra próbálkozna.',
+    'token' => 'Jelszó visszaállító kulcs helytelen.',
+    'user' => "Nem találtunk felhazsnálót ezzel az e-mail címmel.",
+
+];
